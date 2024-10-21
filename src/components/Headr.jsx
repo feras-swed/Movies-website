1ad0bea02dc22b708c70a2575dffa0209e969c88
Feras Swed <65608396+feras-swed@users.noreply.github.com>
2024-10-22 02:09:54 +0300

    feat: Add Headr

---
+++ src/components/Headr.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'next/link';
+import ThemeSwitcher from '@/components/ThemeSwitcher';
+import MovieLists from './MovieLists';
+import GenreLists from './GenreLists';
+import LanguageSwitcher from './LanguageSwitcher';
+
+export default function Headr({lang}) {
+  return (
+    <header className="container mx-auto flex justify-between items-center py-4 bg-background-light dark:bg-background-dark">
+      <h1 className="text-3xl font-bold text-primary-light md:text-2xl dark:text-primary-dark">
+        SWED-MOVIES
+      </h1> 
+      <nav className="flex space-x-4"> 
+
+        <GenreLists/>
+
+        <MovieLists/>
+
+        <Link href="#" className="text-gray-800 hover:text-primary-light dark:text-gray-200 dark:hover:text-primary-active">
+          الممثلون
+        </Link>
+
+      </nav>
+      <LanguageSwitcher lang={lang}/>
+      <div> 
+        <input 
+          type="text" 
+          placeholder="ابحث عن فيلم..." 
+          className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary-light dark:bg-gray-900 dark:border-gray-700 dark:focus:ring-primary-dark" 
+        />
+      </div>
+
+      <ThemeSwitcher />
+    </header>
+  );
+}
---
+++ src/components/MovieLists.jsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+export default function MovieLists() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <button 
+        onClick={toggleDropdown}
+        className="inline-flex w-full justify-between items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 dark:hover:text-primary-hover hover:text-primary-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:focus:ring-primary-dark"
+      >
+        قوائم الأفلام
+        <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
+      </button> 
+
+      {isOpen && (
+        <div className=" absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800 dark:text-gray-300">
+          <ul className="py-1 " role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            <li className="">
+              <Link 
+                href="#" 
+                className="dark:hover:text-primary-hover hover:text-primary-hover block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  dark:hover:bg-gray-700 " 
+                role="menuitem"
+                onClick={() => setIsOpen(false)} // إغلاق القائمة عند اختيار عنصر
+              >
+                الأعلى تقييمًا
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href="#" 
+                className="dark:hover:text-primary-hover hover:text-primary-hover block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  dark:hover:bg-gray-700 " 
+                role="menuitem"
+                onClick={() => setIsOpen(false)} // إغلاق القائمة عند اختيار عنصر
+              >
+                الشعبي
+              </Link>
+            </li>
+            {/* ... باقي قوائم الأفلام ... */}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}

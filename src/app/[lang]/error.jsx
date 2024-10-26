899fcbdfa4271993b3525d100f7edbfa8928bc33
Feras Swed <65608396+feras-swed@users.noreply.github.com>
2024-10-26 23:19:10 +0300

    Feat:Add Loding From Next

--- src/app/[lang]/error.jsx
+++ src/app/[lang]/error.jsx
@@ -8,7 +8,7 @@ export default function Error({ error, reset }) {
   return (
     <div className='text-center mt-10'>
       <h1>Something went wrong. Please try again later.</h1>
-      <button className='hover:text-amber-600' onClick={() => reset()}>
+      <button className='hover:text-primary-hover' onClick={() => reset()}>
         Try Again
       </button>
     </div>

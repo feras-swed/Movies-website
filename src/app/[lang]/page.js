6cc90c882fe1763828f77deac60fcd6f6f13561c
Feras Swed <65608396+feras-swed@users.noreply.github.com>
2024-10-28 06:23:18 +0300

    Fix: Convert image template

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -26,6 +26,7 @@ export default async function Home({ searchParams, }) {
     id: movie.id,
     title: movie.title,
     imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+    imageBackGrondUrl: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
     genre: movie.adult ? 'Adult' : 'Kids',
     additionalInfo: movie.release_date.split('-')[0], 
   }));
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ export default function Card({ result }) {
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
       <Link href={`/movie/${result.id}`}>
-        <img
-          src={`${result.imageUrl}`}
+         <Image
+          src={`${result.imageUrl || result.poster_path}`}
           width={500}
           height={300} 
           alt=''
           className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
-        ></img>
+        ></Image>
         <div className='p-2'>
           <p className='line-clamp-2 text-md'>{result.overview}</p>
           <h2 className='text-lg font-bold truncate'>
